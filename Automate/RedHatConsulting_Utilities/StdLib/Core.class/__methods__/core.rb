53346108a60fb9aee1de8bbfbd2c2c62da55a80f
Jeff Warnica <jwarnica@redhat.com>
2018-05-29 14:09:00 -0300

    Adding some RBAC methods to the core stdlib

--- Automate/RedHatConsulting_Utilities/StdLib/Core.class/__methods__/core.rb
+++ Automate/RedHatConsulting_Utilities/StdLib/Core.class/__methods__/core.rb
@@ -44,6 +44,53 @@ module RedHatConsulting_Utilities
         provider ? (return provider) : (return nil)
       end
 
+      ##
+      # Builds the RBAC array for further checking
+      def get_current_group_rbac_array
+        raise 'get_current_group_rbac_array requires @user to be set' if @user.nil
+        @rbac_array = []
+
+        unless @user.current_group.filters.blank?
+          @user.current_group.filters['managed'].flatten.each do |filter|
+            next unless /(?<category>\w*)\/(?<tag>\w*)$/i =~ filter
+            @rbac_array << {category => tag}
+          end
+        end
+        log(:info, "@user: #{@user.userid} RBAC filters: #{@rbac_array}") if @debug
+        @rbac_array
+      end
+
+      ##
+      # Checks to see if the current user has access to the requested category/tag
+      # Params:
+      # :category: string category name
+      # :tag: string tag name
+      def has_access_to_tag?(category, tag)
+        log(:info, "Searching for access to [#{category}]/[#{tag}]") if @debug
+        get_current_group_rbac_array if @rbac_array.nil?
+        @rbac_array.each do |rbac_hash|
+          #each array element only has a single hash cat=>value. So this loops only once.
+          rbac_hash.each do |rbac_category, rbac_tags|
+            return true if category == rbac_category && tag == rbac_tags
+          end
+        end
+        false
+      end
+
+      # using the rbac filters check to ensure that templates, clusters, security_groups, etc... are tagged
+      def object_eligible?(obj)
+        log(:info, "@user: #{@user.userid} RBAC filters: #{@rbac_array}")
+        log(:info, "obj: [#{obj}, obj tags: [#{obj.tags}]")
+        @rbac_array.each do |rbac_hash|
+          log(:info, "\trh: [#{rbac_hash}]")
+          rbac_hash.each do |rbac_category, rbac_tags|
+            log(:info, "\t\tc: [#{rbac_category}], t: [#{rbac_tags}]")
+            Array.wrap(rbac_tags).each {|rbac_tag_entry| return false unless obj.tagged_with?(rbac_category, rbac_tag_entry)}
+          end
+          true
+        end
+      end
+
     end
   end
 end
