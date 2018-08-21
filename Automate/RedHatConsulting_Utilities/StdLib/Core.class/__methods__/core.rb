0709b3c786b189585c5c2ad630c4107b4369cd4b
Jeff Warnica <jwarnica@redhat.com>
2018-08-21 15:22:08 -0300

    Fix merge updates

--- Automate/RedHatConsulting_Utilities/StdLib/Core.class/__methods__/core.rb
+++ Automate/RedHatConsulting_Utilities/StdLib/Core.class/__methods__/core.rb
@@ -7,11 +7,12 @@ module RedHatConsulting_Utilities
 
       def initialize(handle = $evm)
         @handle = handle
+        @task = get_stp_task
       end
 
       def log(level, msg, update_message = false)
         @handle.log(level, "#{msg}")
-        @handle.task.message = msg if @task && (update_message || level == 'error')
+        @task.message = msg if @task && (update_message || level == 'error')
       end
 
       def dump_thing(thing)
@@ -42,6 +43,44 @@ module RedHatConsulting_Utilities
           log(:info, "Found amazon: #{provider.name} via default method") if provider && use_default
         end
         provider ? (return provider) : (return nil)
+      end
+
+      def get_stp_task
+        task = @handle.root['service_template_provision_task']
+        raise 'service_template_provision_task not found' unless task
+        task
+      end
+
+      def get_request
+        miq_request = @handle.vmdb(:miq_request).find_by_id(get_stp_task.miq_request_id)
+        raise 'miq_request not found' unless miq_request
+        miq_request
+      end
+
+      def get_service
+        service = get_stp_task.destination
+        raise 'service not found' unless service
+        service
+      end
+
+      # Useful for Ansible Service Provisioning.
+      def get_extra_vars
+        extra_vars = get_service.job_options[:extra_vars]
+        log(:info, "extra_vars: #{extra_vars.inspect}")
+        extra_vars
+      end
+
+      def set_extra_vars(extra_vars)
+        service = get_service
+
+        # Remove any keys with blank values from extra_vars.
+        extra_vars.delete_if { |k, v| v == '' }
+
+        # Save updated job_options to service.
+        job_options = service.job_options
+        job_options[:extra_vars] = extra_vars
+        service.job_options = job_options
+        log(:info, "extra_vars updated: #{service.job_options[:extra_vars].inspect}")
       end
 
     end
